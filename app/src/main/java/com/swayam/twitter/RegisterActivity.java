5b53167e6d8c0ed48ba93c1e0a371dc114fb31a9
panil8993 <panil8993@gmail.com>
2020-12-13 17:11:14 +0530

    added backend for Login Activity and Register Activity

--- app/src/main/java/com/swayam/twitter/RegisterActivity.java
+++ app/src/main/java/com/swayam/twitter/RegisterActivity.java
@@ -10,51 +10,56 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.parse.LogInCallback;
 import com.parse.ParseException;
-import com.parse.ParseInstallation;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
-public class LoginActivity extends AppCompatActivity {
-    private EditText usernameField,passwordField;
+public class RegisterActivity extends AppCompatActivity {
+    private EditText usernameField,passwordField,emailField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        ParseInstallation.getCurrentInstallation().saveInBackground();
+        setContentView(R.layout.activity_register);
 
         usernameField = findViewById(R.id.username);
         passwordField = findViewById(R.id.password);
+        emailField = findViewById(R.id.email);
+
     }
 
-    public void login(View view){
+    public void signUp(View view){
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
+        String email = emailField.getText().toString();
 
-        if (username.equals("") || password.equals("") ){
-            showErrorMessage("Username and Password can not be empty");
+        if (username.equals("") || password.equals("") || email.equals("")){
+            showErrorMessage("Username, Password and Email can not be empty");
             return;
         }
+
+        ParseUser parseUser = new ParseUser();
+        parseUser.put("email",email);
+        parseUser.put("username",username);
+        parseUser.put("password",password);
 
         ProgressDialog dialog = new ProgressDialog(this);
         dialog.setCancelable(false);
         dialog.setMessage("please wait...");
         dialog.show();
 
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
+        parseUser.signUpInBackground(new SignUpCallback() {
             @Override
-            public void done(ParseUser user, ParseException e) {
+            public void done(ParseException e) {
                 dialog.dismiss();
                 if (e == null){
-                    startActivity(new Intent(LoginActivity.this,UserActivity.class));
-                    finish();
+                    showSuccessMessage("user created");
                 }else {
                     showErrorMessage(e.getMessage());
                 }
             }
         });
+
     }
 
     public void showSuccessMessage(String message){
@@ -62,10 +67,11 @@ public class LoginActivity extends AppCompatActivity {
                 .setCancelable(false)
                 .setTitle("Successful")
                 .setMessage(message)
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Log In", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        openLogin(null);
+                        finish();
                     }
                 })
                 .create()
@@ -80,8 +86,8 @@ public class LoginActivity extends AppCompatActivity {
                 .show();
     }
 
-    public void openRegistration(View view) {
-        startActivity(new Intent(this,RegisterActivity.class));
+    public void openLogin(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
 }
